ea82bc61174ceb8bbae03c7cbeef6759676ffc82
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2023-02-10 19:56:24 +0100

    from rashie

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -129,27 +129,27 @@ function Navbar() {
             </ul>
           </div>
 
-     <div className="">
-     <div >
-      <form
-            action=""
-            className={` relative items-center m-auto  lg:block   hidden `}
-          >
-            <Input
-              type="text "
-              className={` Input relative peer z-10 bg-transparent w-12 h-12 rounded-full border focus:w-full  transition  focus:border-lime-400 focus:cursor-text pl-12 focus:pr-4 focus:pl-1g   `}
-            />
+          <div className="">
+            <div>
+              <form
+                action=""
+                className={` relative items-center m-auto  lg:block   hidden `}
+              >
+                <Input
+                  type="text "
+                  className={` Input relative peer z-10 bg-transparent w-12 h-12 rounded-full border focus:w-full  transition  focus:border-lime-400 focus:cursor-text pl-12 focus:pr-4 focus:pl-1g   `}
+                />
 
-            <FiSearch className="absolute top-0 bottom-0 inset-y-0 h-8 w-12 my-auto px-3.5 pt-0 items-center   border-r border-transparent peer-focus:border-lime-300  stroke-gray-500  peer-focus:stroke-gray-500  " />
-          </form>
-      </div>
-     </div>
+                <FiSearch className="absolute top-0 bottom-0 inset-y-0 h-8 w-12 my-auto px-3.5 pt-0 items-center   border-r border-transparent peer-focus:border-lime-300  stroke-gray-500  peer-focus:stroke-gray-500  " />
+              </form>
+            </div>
+          </div>
         </Sidebar>
 
         {/* sidebar opens on click of the button but it does not close on click of the button again */}
 
         <nav className={navbar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
+          <ul className="nav-menu-items">
             <li className="navbar-toggle">
               <Link to="#" className="menu-bars">
                 <img src={Logo} className="  " alt="JRGroups Logo" />
